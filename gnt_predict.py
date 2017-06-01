3e5425ebca9c1dfa4c56eb95f6351a280d894d85
Andrew Stromme <andrew.stromme@gmail.com>
2017-06-01 14:21:08 -0700

    a working model, yay

--- gnt_predict.py
+++ gnt_predict.py
@@ -22,7 +22,7 @@ with open(image_filename, 'rb') as f:
 
 
 image_size_const = tf.constant((IMAGE_HEIGHT, IMAGE_WIDTH), dtype=tf.int32)
-x = tf.placeholder(tf.float32, shape=(BATCH_SIZE, None, None, 1))
+x = tf.placeholder(tf.uint8, shape=(BATCH_SIZE, None, None, 1))
 resized_x = tf.image.resize_images(images=x,
                                           size=image_size_const
                                           )
@@ -34,7 +34,7 @@ scaled_x = resized_padded_x / PIXEL_DEPTH - 0.5
 
 
 logits = model(scaled_x)
-prediction = tf.nn.softmax(logits)
+predict_x = tf.nn.softmax(logits)
 
 saver = tf.train.Saver()
 
@@ -49,7 +49,7 @@ image_queue_normalized = image_queue / PIXEL_DEPTH - 0.5
 queue_logits = model(image_queue_normalized)
 queue_prediction = tf.nn.softmax(queue_logits)
 loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
-  labels=label_queue, logits=logits))
+  labels=label_queue, logits=queue_logits))
 
 with open('label_keys.list', encoding='utf8') as f:
     labels_to_char = f.read().split('\n')
@@ -72,36 +72,50 @@ with tf.Session() as sess:
     images, l, normalized_images, labels, predictions = sess.run([image_queue, loss, image_queue_normalized, label_queue, queue_prediction],
         feed_dict={x: numpy.zeros((BATCH_SIZE, 1, 1, 1))})
 
-    print(labels[0])
-    print(labels_to_char[labels[0]])
-
     image1 = images[0, :, :, :]
     h, w, d = image1.shape
+
+    import png
+    #print(image1.reshape(h, w).astype(numpy.uint8))
+    png.from_array(image1.reshape(h, w).astype(numpy.uint8), 'L').save("test.png")
+
     # io.imshow(image1.reshape(h, w))
     # io.show()
     #
     # io.imshow(normalized_images[0, :, :, :].reshape(28, 28))
     # io.show()
 
-    #predictions = sess.run([prediction], feed_dict={x: images})
-    #for index in numpy.argsort(predictions[0][0])[-1:]:
-    index = numpy.argmax(predictions[0][0], 0)
-    print(index)
-    print(labels_to_char[index])
-
-
-    print(len(labels), len(predictions))
     for label, prediction in zip(labels, predictions):
-        top5 = numpy.argsort(predictions[0][0])[-10:]
+        top5 = reversed(numpy.argsort(prediction)[-10:])
+        print([labels_to_char[i] for i in top5])
 
         index = numpy.argmax(prediction)
-        print(label, labels_to_char[label], index, labels_to_char[index], label in top5)#, prediction)
+        print(label, labels_to_char[label], index, labels_to_char[index], label == index)#, prediction)
 
     print(error_rate(predictions, labels))
     print(l)
-    # height, width = image_2d.shape
-    # predictions = sess.run([prediction], feed_dict={x: image_2d.reshape((1, height, width, 1))})
+
+    #image_2d = image_2d * 255
+    print(image_2d.shape)
+    height, width = image_2d.shape
+    predictions, images_2d_resized = sess.run([predict_x, resized_padded_x], feed_dict={x: image_2d.reshape((1, height, width, 1))})
     #
+    io.imshow(image_2d.reshape(height, width))
+    io.show()
+
+    image_2d_resized = images_2d_resized[0, :, :, :]
+    print(image_2d_resized.shape)
+    h, w, _ = image_2d_resized.shape
+    png.from_array(image_2d_resized.reshape(h, w).astype(numpy.uint8), 'L').save("test.png")
+
+    for prediction in predictions:
+        print(prediction.shape)
+        top5 = reversed(numpy.argsort(prediction[0])[-10:])
+        print([labels_to_char[i] for i in top5])
+
+        index = numpy.argmax(prediction)
+        print(index, labels_to_char[index])
+
     # for index in numpy.argsort(predictions[0][0])[-1:]:
     #     print(labels_to_char[index])
 
